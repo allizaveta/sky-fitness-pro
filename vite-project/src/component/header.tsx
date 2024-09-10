5ca723b68fd08f893cc733f231e6e1b96c44a6e8
Elena Rybakova <lelkya84@gmail.com>
2024-09-10 23:10:27 +0300

    main верстка

--- vite-project/src/component/header.tsx
+++ vite-project/src/component/header.tsx
@@ -1,11 +1,13 @@
 export function Header() {
   return (
-    <header className="flex justify-around pt-[40px] pb-[39px] md:pt-[50px] md:pb-[60px]">
+    <header className="flex justify-between pt-[40px] pb-[39px] laptop:pt-[50px] laptop:pb-[60px]">
       <div className="flex flex-col">
         <img src="/logo (1).svg" className="w-[220px] h-auto pb-[15px]" />
-        <p className="hidden md:block ">Онлайн-тренировки для занятий дома</p>
+        <p className="hidden laptop:block opacity-50">
+          Онлайн-тренировки для занятий дома
+        </p>
       </div>
-      <button className="bg-custom-green rounded-full w-[83px] h-[36px] md:w-[103px] md:h-[52px]  hover:bg-hover-green active:bg-active-green self-center">
+      <button className="bg-custom-green rounded-full w-[83px] h-[36px] laptop:w-[103px] laptop:h-[52px]  hover:bg-hover-green active:bg-active-green self-center text-lg font-normal leading-5 text-center active:text-white">
         Войти
       </button>
     </header>
--- vite-project/src/pages/Layout.tsx
+++ vite-project/src/pages/Layout.tsx
@@ -2,12 +2,12 @@ import { Outlet } from "react-router-dom";
 import { Header } from "../component/header";
 
 export function Layout() {
-    return (
-        <div className="bg-bg pr-pd-s pl-pd-s md:pr-pd-lg md:pl-pd-lg">
-            <Header />
-            <main>
-                <Outlet />
-            </main>
-        </div>
-    )
+  return (
+    <div className="bg-bg pr-pd-s pl-pd-s laptop:pr-pd-lg laptop:pl-pd-lg">
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  );
 }
