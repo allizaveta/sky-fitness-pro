aa6346694d155f138f7d2208014f20c59da3cb23
Allizaveta <allizaveta@gmail.com>
2024-09-19 21:51:13 +0300

    модалки закрываются при нажатии на escape

--- src/utils/ModalWrapper.tsx
+++ src/utils/ModalWrapper.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
 
 interface ModalWrapperProps {
   containerClassName?: string;
@@ -9,6 +10,27 @@ export function ModalWrapper({
   containerClassName,
   children,
 }: ModalWrapperProps) {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    const handleEscape = (event: KeyboardEvent) => {
+      if (event.key === "Escape") {
+        // Получаем текущий URL
+        const currentPath = location.pathname;
+        // Ищем последний слэш и удаляем все после него
+        const newPath = currentPath.substring(0, currentPath.lastIndexOf("/"));
+        // Если новый путь пустой, заменяем его на "/"
+        navigate(newPath || "/");
+      }
+    };
+    window.addEventListener("keydown", handleEscape);
+
+    return () => {
+      window.removeEventListener("keydown", handleEscape);
+    };
+  }, [location.pathname, navigate]);
+
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-45">
       <div
