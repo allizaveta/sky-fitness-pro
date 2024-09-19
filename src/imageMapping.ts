9fa68a5953602d64aaafade3487b899f4f8c1af9
Allizaveta <allizaveta@gmail.com>
2024-09-19 23:17:01 +0300

    картинки отображаются соответственно курсу

--- src/imageMapping.ts
+++ src/imageMapping.ts
@@ -5,3 +5,11 @@ export const imageMappings: { [key: string]: string } = {
   "Степ-аэробика": "/MaskGroup(4).png",
   Йога: "/MaskGroup(5).png",
 };
+
+export const directionImages: { [key: string]: string } = {
+  "Танцевальный фитнес": "/skillCard1.png",
+  Бодифлекс: "/skillCard2.png",
+  Стретчинг: "/skillCard3.png",
+  "Степ-аэробика": "/skillCard4.png",
+  Йога: "/skillCard5.png",
+};
