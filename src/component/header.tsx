959f2107cdb49724d966e53e067641fa26b01154
Allizaveta <allizaveta@gmail.com>
2024-09-20 00:37:54 +0300

    лого в хедере возвращает на главную

--- src/component/header.tsx
+++ src/component/header.tsx
@@ -1,4 +1,6 @@
+import { Link } from "react-router-dom";
 import { useAuthorizationModal } from "../context/AuthorizationContext";
+import RoutesPath from "../RoutesPath";
 
 export function Header() {
   const { openModal } = useAuthorizationModal();
@@ -6,7 +8,9 @@ export function Header() {
     <>
       <header className="flex justify-between pt-[40px] pb-[39px] laptop:pt-[50px] laptop:pb-[60px]">
         <div className="flex flex-col">
-          <img src="/logo (1).svg" className="w-[220px] h-auto pb-[15px]" />
+          <Link to={RoutesPath.HOME}>
+            <img src="/logo (1).svg" className="w-[220px] h-auto pb-[15px]" />
+          </Link>
           <p className="hidden laptop:block opacity-50">
             Онлайн-тренировки для занятий дома
           </p>
