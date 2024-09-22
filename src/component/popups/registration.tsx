1d04de70e41847468fae27060242f4e797fa010f
Elena Rybakova <lelkya84@gmail.com>
2024-09-22 21:54:52 +0300

    fix 2

--- src/component/popups/registration.tsx
+++ src/component/popups/registration.tsx
@@ -1,8 +1,57 @@
 import { useAuthorizationModal } from "../../context/AuthorizationContext";
 import { ModalWrapper } from "../../utils/ModalWrapper";
+import RoutesPath from "../../RoutesPath";
+import { useState } from "react";
+import { register } from "../../api";
 
 export function Registration() {
-  const { openModal, closeRegistrationModal } = useAuthorizationModal();
+  const navigate = useNavigate();
+  const [error, setError] = useState("");
+  const [registerData, setRegisterData] = useState({
+    email: "",
+    password: "",
+    passwordAgain: "",
+  });
+
+  function handleRegistration(e: React.ChangeEvent<HTMLInputElement>) {
+    const { name, value } = e.target;
+
+    setRegisterData({
+      ...registerData,
+      [name]: value,
+    });
+    if (registerData.password !== registerData.passwordAgain) {
+      setError("Пароли не совпадают");
+      return;
+    }
+    setError("");
+  }
+
+  function onButton() {
+    if (registerData.password !== registerData.passwordAgain) {
+      setError("Пароли не совпадают");
+      return;
+    }
+    setError("");
+
+    register(registerData.email, registerData.password).then((userData) => {
+      if (userData) {
+        navigate(RoutesPath.LOGIN);
+      } else {
+        setError("Ошибка в регистрации, попробуйте позднее.");
+      }
+    }).catch((error) => {
+      console.log(error.message);
+      if (error.message === "Firebase: Error (auth/email-already-in-use).") {
+        setError("Данная почта уже используется. Попробуйте войти");
+      } else if (error.mesage === "Firebase: Error (auth/network-request-failed).") {
+        setError("Ошибка соединения, попробуйте позднее.");
+      } else {
+        setError("Неопознанная ошибка, попробуйте позднее.");
+      }
+    })
+  }
+
   return (
     <ModalWrapper containerClassName="w-[360px] h-[487px] pd-lg">
       <div className="bg-white w-[360px] h-[487px] pd-lg rounded-[30px] flex flex-col items-center">
@@ -15,31 +64,38 @@ export function Registration() {
             placeholder="Эл. почта"
             className="w-[280px] h-[52px] px-[18px] py-[16px] rounded-[8px] border border-[#D0CECE] opacity-100 mb-[10px]"
             type="email"
+            name="email"
+            onChange={handleRegistration}
           />
           <input
             placeholder="Пароль"
             className="w-[280px] h-[52px] px-[18px] py-[16px] rounded-[8px] border border-[#D0CECE] opacity-100 mb-[10px]"
             type="password"
+            name="password"
+            onChange={handleRegistration}
           />
           <input
             placeholder="Повторите пароль"
             className="w-[280px] h-[52px] px-[18px] py-[16px] rounded-[8px] border border-[#D0CECE] opacity-100"
             type="password"
+            name="passwordAgain"
+            onChange={handleRegistration}
           />
         </div>
         {/* скрытое сообщение об ошибке */}
-        <p className="text-error hidden">
-          Данная почта уже используется.
-          <br />
-          <a href="#"> Попробуйте войти.</a>
-        </p>
+        {error && (
+          <p className="text-error">
+            {error}
+            <br />
+            <a href="#"> Попробуйте войти.</a>
+          </p>
+        )}
         <button className="rounded-full bg-custom-green hover:bg-hover-green active:bg-active-green w-[280px] h-[52px] mb-[10px] text-lg font-normal leading-5 active:text-white text-center">
           Зарегистрироваться
         </button>
         <button
           onClick={() => {
-            openModal();
-            closeRegistrationModal();
+            navigate("login")
           }}
           className="rounded-full bg-white hover:bg-hover-white active:bg-active-white w-[280px] h-[52px] text-lg font-normal leading-5 text-center border-[1px] border-black"
         >
