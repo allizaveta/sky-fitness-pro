98db4a0c39d24a882fe97d992751ca24f9ed1b83
Elena Rybakova <lelkya84@gmail.com>
2024-09-22 22:22:20 +0300

    fix 2

--- src/component/header.tsx
+++ src/component/header.tsx
@@ -3,26 +3,23 @@ import { useAuthorizationModal } from "../context/AuthorizationContext";
 import RoutesPath from "../RoutesPath";
 
 export function Header() {
-  const navigate = useNavigate();
 
   return (
-    <>
-      <header className="flex justify-between pt-[40px] pb-[39px] laptop:pt-[50px] laptop:pb-[60px]">
-        <div className="flex flex-col">
-          <Link to={RoutesPath.HOME}>
-            <img src="/logo (1).svg" className="w-[220px] h-auto pb-[15px]" />
-          </Link>
-          <p className="hidden laptop:block opacity-50">
-            Онлайн-тренировки для занятий дома
-          </p>
-        </div>
-        <button
-          onClick={openModal}
-          className="bg-custom-green rounded-full w-[83px] h-[36px] laptop:w-[103px] laptop:h-[52px] hover:bg-hover-green active:bg-active-green self-center text-lg font-normal leading-5 text-center active:text-white"
-        >
-          Войти
-        </button>
-      </header>
-    </>
+    <header className="flex justify-between pt-[40px] pb-[39px] laptop:pt-[50px] laptop:pb-[60px]">
+      <div className="flex flex-col">
+        <Link to={RoutesPath.HOME}>
+          <img src="/logo (1).svg" className="w-[220px] h-auto pb-[15px]" />
+        </Link>
+        <p className="hidden laptop:block opacity-50">
+          Онлайн-тренировки для занятий дома
+        </p>
+      </div>
+      <button
+        onClick={openModal}
+        className="bg-custom-green rounded-full w-[83px] h-[36px] laptop:w-[103px] laptop:h-[52px] hover:bg-hover-green active:bg-active-green self-center text-lg font-normal leading-5 text-center active:text-white"
+      >
+        Войти
+      </button>
+    </header>
   );
 }
