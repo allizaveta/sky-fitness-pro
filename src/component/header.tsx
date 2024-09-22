c31e8b71035a9dfbd7f7b10c3edba22ea76be4d8
Elena Rybakova <lelkya84@gmail.com>
2024-09-22 23:07:30 +0300

    fix 3??

--- src/component/header.tsx
+++ src/component/header.tsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
 import { useAuthorizationModal } from "../context/AuthorizationContext";
+import { useUserContext } from "../context/userContext";
 import RoutesPath from "../RoutesPath";
+import { useState } from "react";
 
 export function Header() {
   const { currentUser, handleLogout } = useUserContext();
@@ -8,7 +10,6 @@ export function Header() {
 
   const toggleDropdown = () => setDropdownOpen(!dropdownOpen);
   return (
-    <>
       <header className="flex justify-between pt-[40px] pb-[39px] laptop:pt-[50px] laptop:pb-[60px]">
         <div className="flex flex-col">
           <Link to={RoutesPath.HOME}>
@@ -18,13 +19,45 @@ export function Header() {
             Онлайн-тренировки для занятий дома
           </p>
         </div>
-        <button
-          onClick={openModal}
-          className="bg-custom-green rounded-full w-[83px] h-[36px] laptop:w-[103px] laptop:h-[52px] hover:bg-hover-green active:bg-active-green self-center text-lg font-normal leading-5 text-center active:text-white"
-        >
-          Войти
-        </button>
+        {currentUser ? (
+          <div className="relative flex flex-row items-center">
+            <img src="/profilePic.svg" className="h-[42px] w-[42px]"></img>
+            <button
+              onClick={toggleDropdown}
+              className="text-lg font-normal ml-[15px]"
+            >
+              {currentUser.email}
+            </button>
+            <img src="/arrow.svg" className="ml-[5px]" />
+            {dropdownOpen && (
+              <div className="absolute right-[0px] top-[50px] bg-white shadow-lg rounded-[30px] h-[250px] w-[258px] bg-white mt-[24px]">
+                <div className="flex flex-col nowrap justify-center items-center gap-2.5 mt-[50px]">
+                  <p className="font-['StratosSkyeng'] text-lg font-normal leading-[19.8px] text-left text-gray-400 text-gray mb-[24px]">
+                    {currentUser.email}
+                  </p>
+                  <Link to={`${RoutesPath.PROFILE}/${currentUser.uid}`}>
+                    <button className="bg-custom-green rounded-full w-[206px] h-[46px] hover:bg-hover-green active:bg-active-green self-center text-lg font-normal leading-5 text-center active:text-white">
+                      Мой профиль
+                    </button>
+                  </Link>
+                  <button
+                    className="rounded-full bg-white hover:bg-hover-white active:bg-active-white w-[206px] h-[46px] text-lg font-normal leading-5 text-center border-[1px] border-black"
+                    onClick={handleLogout}
+                  >
+                    Выйти
+                  </button>
+                </div>
+              </div>
+            )}
+          </div>
+        ) : (
+          <button
+            onClick={() => openModal()}
+            className="bg-custom-green rounded-full w-[83px] h-[36px] laptop:w-[103px] laptop:h-[52px] hover:bg-hover-green active:bg-active-green self-center text-lg font-normal leading-5 text-center active:text-white"
+          >
+            Войти
+          </button>
+        )}
       </header>
-    </>
   );
 }
