1739fc5908f803c39e94d47247f8d99890975ea1
Elena Rybakova <lelkya84@gmail.com>
2024-09-22 22:22:59 +0300

    fix 3

--- src/component/header.tsx
+++ src/component/header.tsx
@@ -3,6 +3,7 @@ import { useAuthorizationModal } from "../context/AuthorizationContext";
 import RoutesPath from "../RoutesPath";
 
 export function Header() {
+  const { openModal } = useAuthorizationModal();
 
   return (
     <header className="flex justify-between pt-[40px] pb-[39px] laptop:pt-[50px] laptop:pb-[60px]">
