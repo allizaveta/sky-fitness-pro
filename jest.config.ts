c48cd8e50d00794bbd48496e327db1569d3a8186
Allizaveta <allizaveta@gmail.com>
2024-09-30 17:53:13 +0300

    ССССССССАСАМЬААА КПУИРЙ ПАБЛИК

--- jest.config.ts
+++ jest.config.ts
@@ -2,8 +2,12 @@ export default {
   preset: "ts-jest",
   testEnvironment: "jest-environment-jsdom",
   transform: {
-    "^.+\\.tsx?$": "ts-jest",
-    // process `*.tsx` files with `ts-jest`
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        tsconfig: "tsconfig.app.json",
+      },
+    ],
   },
   moduleNameMapper: {
     "\\.(gif|ttf|eot|svg|png)$": "<rootDir>/test/__ mocks __/fileMock.js",
--- src/__test__/authorization.test.tsx
+++ src/__test__/authorization.test.tsx
@@ -1,12 +1,23 @@
 import "@testing-library/jest-dom";
 import { render } from "@testing-library/react";
 import { Authorization } from "../component/popups/authorization";
+import ReduxProvider from "../store/reduxProvider";
+import { MemoryRouter } from "react-router-dom";
+import { AuthorizationProvider } from "../context/AuthorizationContext";
 
 test("demo", () => {
   expect(true).toBe(true);
 });
 
 test("Renders the main page", () => {
-  render(<Authorization />);
+  render(
+    <ReduxProvider>
+      <MemoryRouter>
+        <AuthorizationProvider>
+          <Authorization />
+        </AuthorizationProvider>
+      </MemoryRouter>
+    </ReduxProvider>
+  );
   expect(true).toBeTruthy();
 });
